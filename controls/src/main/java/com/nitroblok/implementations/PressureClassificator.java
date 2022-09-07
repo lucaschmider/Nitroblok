af5149bda88e029e7ad72aab682905efee04214c
felixsommerer <sommeref@dhbw-loerrach.de>
2022-09-07 11:14:58 +0200

    method shoulCreateLog()

--- controls/src/main/java/com/nitroblok/implementations/PressureClassificator.java
+++ controls/src/main/java/com/nitroblok/implementations/PressureClassificator.java
@@ -4,10 +4,29 @@ import com.nitroblok.IPressureClassificator;
 
 public class PressureClassificator implements IPressureClassificator {
 
+    private double logVal = 200.0; //120
+
     @Override
     public boolean shouldCreateLog(double pressure) {
-        // TODO Auto-generated method stub
-        return false;
+        if
+        (
+            (logVal >= 50.0 && pressure < 50.0) || 
+            (logVal <= 50.0 && pressure > 50.0) || 
+            (logVal > 180.0 && pressure <= 180.0) || 
+            (logVal <= 180.0 && pressure > 180.0) ||
+            (logVal < 220.0 && pressure >= 220.0) || 
+            (logVal >= 220.0 && pressure < 220.0) ||
+            (logVal <= 300.0 && pressure > 300) ||   
+            (logVal > 300.0 && pressure <= 300.0) || 
+            (logVal <= 500.0 && pressure > 500) ||   
+            (logVal > 500.0 && pressure <= 500.0) 
+        ) {
+            this.logVal = pressure;
+            return true;
+        } else {
+            this.logVal = pressure;
+            return false;
+        }
     }
 
     @Override
