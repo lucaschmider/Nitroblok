02c47c2711ea653b7d23c4c7339d4bbd03aecdaa
Nick Schupp <schuppn@dhbw-loerrach.de>
2022-09-08 14:40:01 +0200

    finalize tests

--- controls/src/main/java/com/nitroblok/implementations/PressureClassificator.java
+++ controls/src/main/java/com/nitroblok/implementations/PressureClassificator.java
@@ -11,38 +11,35 @@ public class PressureClassificator implements IPressureClassificator {
     public boolean shouldCreateLog(double pressure, double voltage) {
 
         boolean previousPressureIsAccectable = ((logVal > 180) && (logVal < 220)) ? true : false;
-        boolean previousPressureIsCriticallyLow = (logVal < 50.0) ? true : false;
-        boolean previousPressureIsLow = ((logVal >= 50.0) && (logVal <= 180.0)) ? true : false;
-        boolean previousPressureIsHigh = ((logVal >= 220.0) && (logVal <= 300.0)) ? true : false;
-        boolean previousPressureisCriticallyHigh = ((logVal > 300.0) && (logVal <= 500.0)) ? true : false;
-        boolean previousPressureisHazardValue = (logVal > 500.0) ? true : false;
-        
+        boolean previousPressureIsCriticallyLow = (logVal < 50) ? true : false;
+        boolean previousPressureIsLow = ((logVal >= 50) && (logVal <= 180)) ? true : false;
+        boolean previousPressureIsHigh = ((logVal >= 220) && (logVal <= 300)) ? true : false;
+        boolean previousPressureisCriticallyHigh = ((logVal > 300) && (logVal <= 500)) ? true : false;
+        boolean previousPressureisHazardValue = (logVal > 500) ? true : false;
+
         boolean pressureIsAccectable = ((pressure > 180) && (pressure < 220)) ? true : false;
-        boolean pressureIsCriticallyLow = (pressure < 50.0) ? true : false;
-        boolean pressureIsLow = ((pressure >= 50.0) && (pressure <= 180.0)) ? true : false;
-        boolean pressureIsHigh = ((pressure >= 220.0) && (pressure <= 300.0)) ? true : false;
-        boolean pressureIsCriticallyHigh = ((pressure > 300.0) && (pressure <= 500.0)) ? true : false;
-        boolean pressureisHazardValue = (pressure > 500.0) ? true : false;
+        boolean pressureIsCriticallyLow = (pressure < 50) ? true : false;
+        boolean pressureIsLow = ((pressure >= 50) && (pressure <= 180)) ? true : false;
+        boolean pressureIsHigh = ((pressure >= 220) && (pressure <= 300)) ? true : false;
+        boolean pressureIsCriticallyHigh = ((pressure > 300) && (pressure <= 500)) ? true : false;
+        boolean pressureisHazardValue = (pressure > 500) ? true : false;
+
+        boolean previousVoltageIsAcceptable = (logVol >= 5) ? true : false;
+        boolean voltageIsAcceptable = (voltage >= 5) ? true : false;
 
         this.logVal = pressure;
-
-        if(pressureIsAccectable && !previousPressureIsAccectable) return true;
-        if(pressureIsCriticallyLow && !previousPressureIsCriticallyLow) return true;
-        if(pressureIsLow && !previousPressureIsLow) return true;
-        if(pressureIsHigh && !previousPressureIsHigh) return true;
-        if(pressureIsCriticallyHigh && !previousPressureisCriticallyHigh) return true;
-        if(pressureisHazardValue && !previousPressureisHazardValue) return true;
-
-
-        boolean previousVoltageIsAcceptable = (logVol >= 5.0) ? true : false;
-        boolean previousVoltageWithDefect = (logVol < 5.0) ? true : false;
-        boolean voltageIsAcceptable = (voltage >= 5.0) ? true : false;
-        boolean voltageWithDefect = (voltage < 5.0) ? true : false;
-
         this.logVol = voltage;
 
-        if(voltageIsAcceptable && !previousVoltageIsAcceptable) return true;
-        if(voltageWithDefect && !previousVoltageWithDefect) return true;
+        //check Pressure
+        if(pressureIsAccectable != previousPressureIsAccectable) return true;
+        if(pressureIsCriticallyLow != previousPressureIsCriticallyLow) return true;
+        if(pressureIsLow != previousPressureIsLow) return true;
+        if(pressureIsHigh != previousPressureIsHigh) return true;
+        if(pressureIsCriticallyHigh != previousPressureisCriticallyHigh) return true;
+        if(pressureisHazardValue != previousPressureisHazardValue) return true;
+
+        //check Voltage
+        if(voltageIsAcceptable != previousVoltageIsAcceptable) return true;
 
 
         return false;
--- controls/src/test/java/com/nitroblok/PressureClassificatorTest.java
+++ controls/src/test/java/com/nitroblok/PressureClassificatorTest.java
@@ -13,48 +13,48 @@ import com.nitroblok.implementations.PressureClassificator;
 
 public class PressureClassificatorTest {
 
-    @ParameterizedTest(name ="{index} => pressure={0}, voltage={1}, expected={2}")
-    @MethodSource("ShouldRing_initializationSource")
-    void shouldCreateLogInitializationMeasurement(double pressure, double voltage, boolean expected) {
+    @ParameterizedTest(name ="{index} => initValue={0}, voltage={1}, expected={2}")
+    @MethodSource("initializationSource")
+    void shouldCreateLogInitializationMeasurement(double initValue, double voltage, boolean expected) {
         IPressureClassificator pressureClassificator = new PressureClassificator();
-        boolean result = pressureClassificator.shouldCreateLog(pressure, voltage);
+        boolean result = pressureClassificator.shouldCreateLog(initValue, voltage);
         assertThat(result, IsEqual.equalTo(expected));
     }
-    static Stream<Arguments> ShouldRing_initializationSource() {
+    static Stream<Arguments> initializationSource() {
         return Stream.of(
-                Arguments.of(45.0, 4.99, true),
-                Arguments.of(45.0, 5, true),
-
-                Arguments.of(120.0, 4.99, true),
-                Arguments.of(120.0, 5, false),
-
-                Arguments.of(186.0, 4.99, true),
-                Arguments.of(186.0, 5, false),
-
-                Arguments.of(248.0, 4.99, true),
-                Arguments.of(248.0, 5, false),
-
-                Arguments.of(310.0, 4.99, true),
-                Arguments.of(310.0, 5, true),
-
-                Arguments.of(540.0, 4.99, true),
-                Arguments.of(540.0, 5, true)
+            Arguments.of(45.0, 4.99, true),
+            Arguments.of(45.0, 5, true),
+            
+            Arguments.of(120.0, 4.99, true),
+            Arguments.of(120.0, 5, true),
+            
+            Arguments.of(186.0, 4.99, true),
+            Arguments.of(186.0, 5, false),
+            
+            Arguments.of(248.0, 4.99, true),
+            Arguments.of(248.0, 5, true),
+            
+            Arguments.of(310.0, 4.99, true),
+            Arguments.of(310.0, 5, true),
+            
+            Arguments.of(540.0, 4.99, true),
+            Arguments.of(540.0, 5, true)
         );
     }
 
-    @ParameterizedTest(name ="{index} => initValue={0}, measuredValue={1}, initVoltage={2}, measuredVoltage={3} expected={4}")
+    @ParameterizedTest(name ="{index} => initPressure={0}, measuredPressure={1}, initVoltage={2}, measuredVoltage={3} expected={4}")
     @MethodSource("priorValuesSource")
-    void shouldCreateLogWithPriorValuesMeausurement(double initValue, double measuredValue, double initVoltage, double measuredVoltage, boolean expected) {
+    void shouldCreateLogWithPriorValuesMeausurement(double initPressure, double measuredPressure, double initVoltage, double measuredVoltage, boolean expected) {
         final IPressureClassificator pressureClassificator = new PressureClassificator();
-        pressureClassificator.shouldCreateLog(initValue, initVoltage);
-        final boolean result = pressureClassificator.shouldCreateLog(measuredValue, measuredVoltage);
+        pressureClassificator.shouldCreateLog(initPressure, initVoltage);
+        final boolean result = pressureClassificator.shouldCreateLog(measuredPressure, measuredVoltage);
         assertThat(result, IsEqual.equalTo(expected));
     }
     static Stream<Arguments> priorValuesSource() {
         return Stream.of(
                 Arguments.of(49.99999, 48.999, 12, 13, false),
-                //Arguments.of(49.99999, 48.999, 4, 3, false),
-                //Arguments.of(49.99999, 48.999, 4.99, 5, true),
+                Arguments.of(49.99999, 48.999, 4, 3, false),
+                Arguments.of(49.99999, 48.999, 4.99, 5, true),
                 Arguments.of(49.99999, 48.999, 5, 4.99, true),
 
                 Arguments.of(45.0, 120.0, 12, 13,  true),
@@ -73,8 +73,8 @@ public class PressureClassificatorTest {
                 Arguments.of(55.0, 49.99999, 5, 4.99, true),
 
                 Arguments.of(120.0, 100.0, 12, 13,  false),
-                //Arguments.of(120.0, 100.0, 4, 3, false),
-                //Arguments.of(120.0, 100.0, 4.99, 5, true),
+                Arguments.of(120.0, 100.0, 4, 3, false),
+                Arguments.of(120.0, 100.0, 4.99, 5, true),
                 Arguments.of(120.0, 100.0, 5, 4.99, true),
 
                 Arguments.of(170.0, 186.0, 12, 13,  true),
@@ -98,8 +98,8 @@ public class PressureClassificatorTest {
                 Arguments.of(186.0, 257.0, 5, 4.99, true),
 
                 Arguments.of(230.0, 289.0, 12, 13,  false),
-                //Arguments.of(230.0, 289.0, 4, 3, false),
-                //Arguments.of(230.0, 289.0, 4.99, 5, true),
+                Arguments.of(230.0, 289.0, 4, 3, false),
+                Arguments.of(230.0, 289.0, 4.99, 5, true),
                 Arguments.of(230.0, 289.0, 5, 4.99, true),
 
                 Arguments.of(257.0, 186.0, 12, 13,  true),
@@ -123,13 +123,13 @@ public class PressureClassificatorTest {
                 Arguments.of(310.0, 510.0, 5, 4.99, true),
 
                 Arguments.of(330.0, 400.0, 12, 13,  false),
-                //Arguments.of(330.0, 400.0, 4, 3, false),
-                //Arguments.of(330.0, 400.0, 4.99, 5, true),
+                Arguments.of(330.0, 400.0, 4, 3, false),
+                Arguments.of(330.0, 400.0, 4.99, 5, true),
                 Arguments.of(330.0, 400.0, 5, 4.99, true),
 
                 Arguments.of(503.0, 700.0, 12, 13,  false),
-                //Arguments.of(503.0, 700.0, 4, 3, false),
-                //Arguments.of(503.0, 700.0, 4.99, 5, true),
+                Arguments.of(503.0, 700.0, 4, 3, false),
+                Arguments.of(503.0, 700.0, 4.99, 5, true),
                 Arguments.of(503.0, 700.0, 5, 4.99, true),
                 
                 Arguments.of(510.0, 310.0, 12, 13,  true),
